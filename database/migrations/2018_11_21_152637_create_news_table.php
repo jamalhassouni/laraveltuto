abcb1a968fd60f5601a035c0eaffb7517f2269ef
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-11-27 16:18:54 +0100

    Connect to the database and recognize migration refresh + fresh + status + rollback commands

--- database/migrations/2018_11_21_152637_create_news_table.php
+++ database/migrations/2018_11_21_152637_create_news_table.php
@@ -15,6 +15,11 @@ class CreateNewsTable extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->integer('add_by');
+            $table->text('desc');
+            $table->longText('content');
+            $table->enum('status',['active','pending','inactive']);
             $table->timestamps();
         });
     }
