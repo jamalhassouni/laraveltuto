b2471861d184af3d5bbda34ca5ce7112f5294b39
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-01 17:27:13 +0100

    fix:MassAssignmentException in Laravel

--- app/News.php
+++ app/News.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
 class News extends Model
 {
     use SoftDeletes;
+    protected $fillable = ['title', 'desc', 'content', 'add_by', 'status'];
     protected $date = ['delete_at'];
 
 }
