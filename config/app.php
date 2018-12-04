0e8f6ada821d46235506ea16f7b0575d83c4f9e9
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-04 22:58:12 +0100

    Install and delete packages

--- config/app.php
+++ config/app.php
@@ -177,6 +177,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /////////////////// My Package ///////////////////
+        Collective\Html\HtmlServiceProvider::class,
+
+        /////////////////// My Package ///////////////////
+
+
     ],
 
     /*
@@ -225,7 +231,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        /////////////////// My Aliases ///////////////////
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        /////////////////// My Aliases ///////////////////
     ],
 
 ];
